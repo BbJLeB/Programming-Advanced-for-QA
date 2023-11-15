b0fadbb35d85c51e2b2fd4c89ba983394dc331de
Димитър Иванов <59648725+BbJLeB@users.noreply.github.com>
2023-11-15 07:21:45 +0200

    03. Strings and Text Processing

---
+++ 1. Reverse Strings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string input;
+
+        while ((input = Console.ReadLine()) != "end")
+        {
+            string reversed = new string(input.Reverse().ToArray());
+            Console.WriteLine($"{input} = {reversed}");
+        }
+    }
+}
---
+++ 2. Repeat Strings.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+string words = string.Concat(
+    Console.ReadLine()
+    .Split(" ")
+    .Select(x => string.Concat(Enumerable.Repeat(x, x.Length))));
+
+Console.WriteLine(words);
+
+//string[] words = Console.ReadLine().Split(" ");
+
+//StringBuilder output = new();
+
+//for (int i = 0; i < words.Length; i++)
+//{
+//    string currentWord = words[i];
+//    for (int j = 0; j < currentWord.Length; j += 1)
+//    {
+//        output.Append(currentWord);
+//    }
+//}
+
+//Console.WriteLine(output);
